daf32a184cb7a81e0a012f870b982e77178b122f
yutannihilation <yutani.ini@gmail.com>
2015-11-08 16:58:19 +0900

    add credit for the code

--- src/src/fib.rs
+++ src/src/fib.rs
@@ -1,6 +1,8 @@
+// original code is here: http://qiita.com/rejasupotaro/items/2aa57a67f4a97101950c
+
 #[no_mangle]
 pub extern fn fib(n: u32) -> u32 {
-    if n <= 1 {
+    if n == 0 || n == 1 {
         n
     } else {
         fib(n - 1) + fib(n - 2)
